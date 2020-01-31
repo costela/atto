20f375cd7833bb39ecde7bbceb293d7ff5920428
Leo Antunes <leo@costela.net>
2020-01-31 19:59:11 +0100

    split config; use structured logging

---
+++ config.go
@@ -0,0 +1,26 @@
+package main
+
+import "time"
+
+var conf = struct {
+	LogLevel string `default:"warn" short:"l" desc:"level of logging output (one of debug/info/warn/error)"`
+	Port     int    `default:"8080" desc:"port to listen on"`
+	Path     string `default:"." desc:"path to serve"`
+	Path404  string `default:"404.html" desc:"path to a file whose content will be returned on 404 (relative to --path)"`
+	Prefix   string `default:"" desc:"prefix under which atto will be accessed (this will be stripped before accessing 'path')"`
+	ShowList bool   `default:"false" desc:"whether to display folder contents"`
+	Compress bool   `default:"true" desc:"whether to transparently compress served files"`
+	Timeout  struct {
+		ReadHeader *duration `default:"5s" desc:"time to wait for request headers"`
+		Shutdown   *duration `default:"30s" desc:"time to wait for ungoing requests to finish before shutting down"`
+	}
+}{}
+
+// work around time.Duration's lack of UnmarshalText
+type duration time.Duration
+
+func (d *duration) UnmarshalText(data []byte) error {
+	dd, err := time.ParseDuration(string(data))
+	*d = duration(dd)
+	return err
+}
--- main.go
+++ main.go
@@ -20,20 +20,6 @@ var version = "devel"
 
 var logger *logrus.Logger
 
-var conf = struct {
-	LogLevel string `default:"warn" short:"l" desc:"level of logging output (one of debug/info/warn/error)"`
-	Port     int    `default:"8080" desc:"port to listen on"`
-	Path     string `default:"." desc:"path to serve"`
-	Path404  string `default:"404.html" desc:"path to a file whose content will be returned on 404 (relative to --path)"`
-	Prefix   string `default:"" desc:"prefix under which atto will be accessed (this will be stripped before accessing 'path')"`
-	ShowList bool   `default:"false" desc:"whether to display folder contents"`
-	Compress bool   `default:"true" desc:"whether to transparently compress served files"`
-	Timeout  struct {
-		ReadHeader *duration `default:"5s" desc:"time to wait for request headers"`
-		Shutdown   *duration `default:"30s" desc:"time to wait for ungoing requests to finish before shutting down"`
-	}
-}{}
-
 func main() {
 	logger = logrus.New()
 
@@ -50,13 +36,17 @@ func main() {
 	}
 	log.SetOutput(logger.Writer())
 
-	logger.WithField("version", version).Debugf("starting atto")
+	logger.WithFields(logrus.Fields{
+		"version": version,
+		"path":    conf.Path,
+	}).Debug("starting atto")
 
-	logger.Debugf("instantiating server for path %s", conf.Path)
 	handler := http.StripPrefix(conf.Prefix, http.FileServer(safeDir(conf.Path)))
+
 	if conf.Compress {
 		handler = gziphandler.GzipHandler(handler)
 	}
+
 	server := http.Server{
 		Addr:              fmt.Sprintf(":%d", conf.Port),
 		Handler:           logHandler{handler},
@@ -66,22 +56,32 @@ func main() {
 	}
 
 	server.RegisterOnShutdown(func() {
-		logger.Debugf("shutting down gracefully (timeout: %s)", time.Duration(*conf.Timeout.Shutdown))
+		logger.WithFields(logrus.Fields{
+			"timeout": time.Duration(*conf.Timeout.Shutdown),
+		}).Debug("shutting down gracefully")
 	})
 
 	wg := sync.WaitGroup{}
+
 	wg.Add(1)
+
 	go func() {
 		defer wg.Done()
-		logger.Debugf("listening on port %d", conf.Port)
+
+		logger.WithFields(logrus.Fields{
+			"port": conf.Port,
+		}).Debug("starting server")
+
 		if err := server.ListenAndServe(); err != http.ErrServerClosed {
 			logger.Fatal(err)
 		}
 	}()
 
 	wg.Add(1)
+
 	go func() {
 		defer wg.Done()
+
 		handleSignals(&server)
 	}()
 
@@ -97,26 +97,22 @@ func (lh logHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	lh.inner.ServeHTTP(w, r)
 }
 
-// work around time.Duration's lack of UnmarshalText
-type duration time.Duration
-
-func (d *duration) UnmarshalText(data []byte) error {
-	dd, err := time.ParseDuration(string(data))
-	*d = duration(dd)
-	return err
-}
-
 func handleSignals(server *http.Server) {
 	signalChan := make(chan os.Signal, 1)
+
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
 	<-signalChan
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*conf.Timeout.Shutdown))
+
 	defer cancel()
+
 	if err := server.Shutdown(ctx); err != nil {
 		if err == context.DeadlineExceeded {
-			logger.Warnf("timeout exceeded while shutting down")
+			logger.Warn("timeout exceeded while shutting down")
 		} else {
-			logger.Errorf("error while shutting down server: %s", err)
+			logger.WithError(err).Error("error while shutting down server")
 		}
 	}
 }
